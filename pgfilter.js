e0a38f75a2a5be525f09e1f09ac645564ee6a9d8
Javier Galarza <jegj57@gmail.com>
2021-12-21 08:54:29 -0500

    Change option generation with a new sample

--- pgfilter.js
+++ pgfilter.js
@@ -1,21 +1,27 @@
 #!/usr/bin/env node
 
 'use strict';
-const pfgilter = require('commander');
+const { Command, Option } = require('commander');
+const pfgilter = new Command();
 
-// const { init } = require('./src/');
+const init = require('./src/');
 
 const PGFILTER_VERSION = require('./package.json').version;
 
 pfgilter
 	.name('pgfilter')
-	.requiredOption('-f, --pgfilter-file <pgfilterFile>', 'Path to the filtering/transformation JSON file')
-	.option('-l, --max-buffer-length <maxBufferLength>', 'Set internal buffer size. There is no limit by default. If set, process will throw an error as soon the buffer exceed the limit. Use --skip-overflow to avoid exit the whole process.', parseInt, undefined)
-	.option('-s, --skip-overflow', 'If set, the line that exceed the internal buffer will be ignored and the process will not exit', false)
-	.option('-d, --debug', 'Show debug messages in stderr', false)
+	.addOption(
+		new Option('-f, --pgfilter-file <pgfilterFile>',
+			'Path to the filtering/transformation JSON file.'
+		).env(
+			'PGFILTER_FILE'
+		).required = true
+	)
+	// .option('-l, --max-buffer-length <maxBufferLength>', 'Set internal buffer size. There is no limit by default. If set, process will throw an error as soon the buffer exceed the limit. Use --skip-overflow to avoid exit the whole process.', parseInt, undefined).env('PGFILTER_BUFFER').option('-s, --skip-overflow', 'If set, the line that exceed the internal buffer will be ignored and the process will not exit', false)
+	// .option('-d, --debug', 'Show debug messages in stderr', false)
 	.arguments('[inputfile]')
 	.version(PGFILTER_VERSION)
 	.usage('[options] [inputfile]')
 	.parse(process.argv);
 
-// init(pfgilter.opts(), pfgilter.args);
+init(pfgilter.opts(), pfgilter.args);
---
+++ src/index.js
@@ -0,0 +1,17 @@
+/* src/index.js */
+'use strict';
+
+
+const init = function _init(options, args = []) {
+
+	console.log('===>', options.pgfilterFile);
+	// const filterFile = getFilterFile(options.pgfilterFile);
+	// if (args.length > 0) {
+	// 	initFileRedirection(options, filterFile, args[0]);
+	// } else {
+	// 	initStdinRedirection(options, filterFile);
+	// }
+};
+
+
+module.exports = init;
