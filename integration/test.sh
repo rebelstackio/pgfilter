3e693a5e77ee58e39dbf66423622d14257b19c33
Javier Galarza <jegj57@gmail.com>
2022-08-17 08:42:45 -0500

    chore(test): fix integration test

--- integration/test.sh
+++ integration/test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-command=$(which pgfilter)
-
-$command -vf vagrant/test/dvdrental.default.json vagrant/backup/dvdrental.dump > dvdrental.transformed.dump
+node pgfilter.js -vf vagrant/test/dvdrental.default.json vagrant/backup/dvdrental.dump > dvdrental.transformed.dump
 
 original_num_lines=$(wc vagrant/backup/dvdrental.dump -l | cut -d ' ' -f 1)
 trans_num_lines=$(wc dvdrental.transformed.dump -l | cut -d ' ' -f 1)
@@ -15,12 +13,11 @@ fi
 
 if cmp -s vagrant/backup/dvdrental.dump dvdrental.transformed.dump; then
   echo "Files are identical. Something wrong happended..."
-  rm dvdrental.transformed.dump
   code=1
 else
-  echo "Same number of lines and file are different"
+  echo "Same number of lines and file are different. Integration test passed"
   code=0
 fi
 
-rm dvdrental.transformed.dump
+rm -f dvdrental.transformed.dump
 exit $code
