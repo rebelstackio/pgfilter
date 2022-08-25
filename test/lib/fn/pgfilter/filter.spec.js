1c8363f16f18179e698324aacd5730c795962e10
Javier Galarza <jegj57@gmail.com>
2022-08-25 08:51:42 -0500

    feat(filter): allow more duration expression on filter function

--- test/lib/fn/pgfilter/filter.spec.js
+++ test/lib/fn/pgfilter/filter.spec.js
@@ -8,7 +8,7 @@ const test = t.test
 const { fnow, ftest } = require('../../../../lib/fn/pgfilter/filter')
 
 test('pgfilter.filter namespace function test suit', (tc) => {
-  tc.plan(12)
+  tc.plan(18)
   tc.test('fnow must throw an exception if the range argument is not valid', (t) => {
     t.plan(4)
     let rangeF = null
@@ -34,7 +34,7 @@ test('pgfilter.filter namespace function test suit', (tc) => {
     })
   })
 
-  tc.test('fnow must return false if the date pass the range ( P30D of 2020-11-09T18:41:14.419Z )', (t) => {
+  tc.test('fnow must return false if the date pass the range ( 30 days/P30D of 2020-11-09T18:41:14.419Z )', (t) => {
     t.plan(1)
     const rangeF = 'P30D'
     const column = '2020-11-09T18:41:14.419Z'
@@ -45,7 +45,7 @@ test('pgfilter.filter namespace function test suit', (tc) => {
     t.notOk(result)
   })
 
-  tc.test('fnow must return true if the date does not pass the range ( P30D of 2010-11-09T18:41:14.419Z )', (t) => {
+  tc.test('fnow must return true if the date does not pass the range ( 30 days/P30D of 2010-11-09T18:41:14.419Z )', (t) => {
     t.plan(1)
     const rangeF = 'P30D'
     const column = '2010-11-09T18:41:14.419Z'
@@ -56,7 +56,7 @@ test('pgfilter.filter namespace function test suit', (tc) => {
     t.ok(result)
   })
 
-  tc.test('fnow must return true if the date does not pass the range ( P2D of 2020-11-05T18:41:14.419Z )', (t) => {
+  tc.test('fnow must return true if the date does not pass the range ( 2 days/P2D of 2020-11-05T18:41:14.419Z )', (t) => {
     t.plan(1)
     const rangeF = 'P2D'
     const column = '2020-11-05 14:22:48.672979-06'
@@ -83,7 +83,7 @@ test('pgfilter.filter namespace function test suit', (tc) => {
     t.ok(result)
   })
 
-  tc.test('fnow must return true if the date does not pass the range( P20D of 2017-11-29T09:45:10.000Z)', (t) => {
+  tc.test('fnow must return true if the date does not pass the range( 20 days/P20D of 2017-11-29T09:45:10.000Z)', (t) => {
     t.plan(1)
     const rangeF = 'P20D'
     const column = '2017-11-29T09:45:10.000Z'
@@ -94,7 +94,7 @@ test('pgfilter.filter namespace function test suit', (tc) => {
     t.ok(result)
   })
 
-  tc.test('fnow must return true if the date does not pass the range( P1Y of 2015-03-03 13:57:54.856896 )', (t) => {
+  tc.test('fnow must return true if the date does not pass the range( 1 year/P1Y of 2015-03-03 13:57:54.856896 )', (t) => {
     t.plan(1)
     const rangeF = 'P1Y'
     const column = '2015-03-03 13:57:54.856896'
@@ -105,7 +105,7 @@ test('pgfilter.filter namespace function test suit', (tc) => {
     t.ok(result)
   })
 
-  tc.test('fnow must return false if the date pass the range ( P1Y of 2020-11-29T09:25:20.000Z )', (t) => {
+  tc.test('fnow must return false if the date pass the range ( 1 year/P1Y of 2020-11-29T09:25:20.000Z )', (t) => {
     t.plan(1)
     const rangeF = 'P1Y'
     const column = '2020-11-29T09:25:20.000Z'
@@ -116,7 +116,7 @@ test('pgfilter.filter namespace function test suit', (tc) => {
     t.notOk(result)
   })
 
-  tc.test('fnow must return false if the date pass the range ( P1Y on a recent date)', (t) => {
+  tc.test('fnow must return false if the date pass the range ( 1 year/P1Y on a recent date)', (t) => {
     t.plan(1)
     const rangeF = 'P1Y'
     const column = '2021-01-01T17:13:46.980Z'
@@ -127,7 +127,7 @@ test('pgfilter.filter namespace function test suit', (tc) => {
     t.notOk(result)
   })
 
-  tc.test('fnow must return true if the date does not pass the range( P1M on 2021-03-01T17:12:46.980Z)', (t) => {
+  tc.test('fnow must return true if the date does not pass the range in hours( 1 month/P1M on 2021-03-01T17:12:46.980Z)', (t) => {
     t.plan(1)
     const rangeF = 'P1M'
     const column = '2021-01-01T17:13:46.980Z'
@@ -138,15 +138,81 @@ test('pgfilter.filter namespace function test suit', (tc) => {
     t.ok(result)
   })
 
-  tc.skip('fnow must return false if the date does pass the range( PT1M on 2021-03-01T17:12:46.980Z)', (t) => {
+  tc.test('fnow must return false if the date pass the range( 1minute/PT1M on 2021-03-01T17:52:46.980Z)', (t) => {
     t.plan(1)
     const rangeF = 'PT1M'
     const column = '2021-03-01T17:52:46.980Z'
 
-    MockDate.set('2021-03-01T17:12:46.980Z')
+    MockDate.set('2021-03-01T17:53:46.980Z')
     const result = fnow(column, rangeF)
 
     t.notOk(result)
+  })
+
+  tc.test('fnow must return false if the date pass the range( 5minutes/PT5M on 2021-03-01T18:52:46.980Z)', (t) => {
+    t.plan(1)
+    const rangeF = 'PT5M'
+    const column = '2021-03-01T18:49:46.980Z'
+
+    MockDate.set('2021-03-01T18:52:46.980Z')
+    const result = fnow(column, rangeF)
+
+    t.notOk(result)
+  })
+
+  tc.test('fnow must return true if the date does not pass the range( 5minutes/PT5M on 2021-03-01T18:52:46.980Z)', (t) => {
+    t.plan(1)
+    const rangeF = 'PT5M'
+    const column = '2021-03-01T18:45:46.980Z'
+
+    MockDate.set('2021-03-01T18:52:46.980Z')
+    const result = fnow(column, rangeF)
+
+    t.ok(result)
+  })
+
+  tc.test('fnow must return false if the date pass the range( 1hour/PT1H on 2021-03-01T17:52:46.980Z)', (t) => {
+    t.plan(1)
+    const rangeF = 'PT1H'
+    const column = '2021-03-01T16:52:46.980Z'
+
+    MockDate.set('2021-03-01T17:52:46.980Z')
+    const result = fnow(column, rangeF)
+
+    t.notOk(result)
+  })
+
+  tc.test('fnow must return true if the date does not pass the range( 1hour/PT1H on 2021-03-01T17:52:46.980Z)', (t) => {
+    t.plan(1)
+    const rangeF = 'PT1H'
+    const column = '2021-03-01T15:52:46.980Z'
+
+    MockDate.set('2021-03-01T17:52:46.980Z')
+    const result = fnow(column, rangeF)
+
+    t.ok(result)
+  })
+
+  tc.test('fnow must return false if the date pass the range( 5second/PT5S on 2021-03-01T17:52:46.980Z)', (t) => {
+    t.plan(1)
+    const rangeF = 'PT5S'
+    const column = '2021-03-01T17:52:43.980Z'
+
+    MockDate.set('2021-03-01T17:52:46.980Z')
+    const result = fnow(column, rangeF)
+
+    t.notOk(result)
+  })
+
+  tc.test('fnow must return true if the date does not pass the range( 5second/PT5S on 2021-03-01T17:52:46.980Z)', (t) => {
+    t.plan(1)
+    const rangeF = 'PT5S'
+    const column = '2021-03-01T17:52:10.980Z'
+
+    MockDate.set('2021-03-01T17:52:46.980Z')
+    const result = fnow(column, rangeF)
+
+    t.ok(result)
   })
 
   tc.test('ftest must return and object with arguments', (t) => {
